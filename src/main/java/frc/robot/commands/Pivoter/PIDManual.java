ea2ec0d7960d916d4f324062435025559876fa06
yifanainanan <yifan99.jiang@gmail.com>
2024-06-05 18:27:17 -0400

    Robot Changes

--- src/main/java/frc/robot/commands/Pivoter/PIDManual.java
+++ src/main/java/frc/robot/commands/Pivoter/PIDManual.java
@@ -31,10 +31,10 @@ public class PIDManual extends Command {
   public void initialize() {
     currentPivoterReading = pivoterSub.getPivoterRawRotation();
     if (isUp){
-      target = currentPivoterReading + 0.5
+      target = currentPivoterReading + 3
       ;//2
     }else{
-      target = currentPivoterReading - 0.5;//1.5
+      target = currentPivoterReading - 2;//1.5
     }
     SmartDashboard.putNumber("Manual Pivoter Target Rotation", target);
 
